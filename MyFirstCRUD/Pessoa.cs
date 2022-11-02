832e4fd28ed8e3e6a0c319734da4ccb2a26358ca
Israel Schroeder Moreira <israelschroederm@gmail.com>
2022-11-02 00:21:44 -0300

    Third Commit
    Added code in classes

--- MyFirstCRUD/Pessoa.cs
+++ MyFirstCRUD/Pessoa.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace MyFirstCRUD
 {
-    internal class Class1
+    internal class Pessoa
     {
+        // Propriedades estáticas
+        public static int id;
+
+        // Propriedades de uma pessoa
+        public string nome;
+        public string email;
+        public int idade;
+        public List<Pessoa> pessoas;
+
+        public Pessoa(string _nome, string _email, int _idade)
+        {
+            this.nome = _nome;
+            this.email = _email;
+            this.idade = _idade;
+
+            id++;
+        }
+
+        public void WritePessoa()
+        {
+            System.Console.WriteLine("Nome: " + this.nome);
+            System.Console.WriteLine("E-mail: " + this.email);
+            System.Console.WriteLine("Idade: " + this.idade);
+            System.Console.WriteLine("O ID da pessoa é:" + id);
+        }
+
+        public void Create()
+        {
+            if (this.nome == "")
+            {
+                System.Console.WriteLine("Digite um nome válido!");
+                return;
+            }else if (this.email == "")
+            {
+                System.Console.WriteLine("Digite um e-mail válido!");
+                return;
+            }else if (this.idade < 0 || this.idade == 0)
+            {
+                System.Console.WriteLine("Digite uma idade válida!");
+                return;
+            }
+            else
+            {
+
+                pessoas.Add(new Pessoa(this.nome, this.email, this.idade));
+            }
+        }
     }
 }
--- MyFirstCRUD/Program.cs
+++ MyFirstCRUD/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using MyFirstCRUD;
+
+// public Pessoa(string _nome, string _email, string _idade, List<Pessoa> _pessoas)
+Pessoa pessoa = new Pessoa("Daniele Schroeder", "danischroederm@gmail.com",19);
+pessoa.Create();
+pessoa.WritePessoa();
